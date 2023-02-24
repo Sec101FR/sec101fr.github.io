72145950a53f82cf872aa0b54d7921e24432de9a
xanhacks <xanhacks@protonmail.com>
2023-02-24 19:18:41 +0100

    initial commit

---
+++ src/config.mjs
@@ -0,0 +1,62 @@
+import defaultImage from './assets/images/default.png';
+
+const CONFIG = {
+  name: 'Sec101',
+
+  origin: 'https://astrowind.vercel.app',
+  basePathname: '/',
+  trailingSlash: false,
+
+  title: 'Sec101 — Prestation d\'audit de cybersécurité, applications web & mobiles, infrastructure cloud.', 
+  description:
+    '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
+  defaultImage: defaultImage,
+
+  defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+
+  language: 'en',
+  textDirection: 'ltr',
+
+  dateFormatter: new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'UTC',
+  }),
+
+  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+  googleSiteVerificationId: '',
+
+  blog: {
+	disabled: false,
+    postsPerPage: 4,
+
+    list: {
+      pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+      noindex: false,
+      disabled: false,
+    },
+
+    post: {
+      pathname: '', // empty for /some-post, value for /pathname/some-post
+      noindex: false,
+      disabled: false,
+    },
+
+    category: {
+      pathname: 'category', // set empty to change from /category/some-category to /some-category
+      noindex: true,
+      disabled: false,
+    },
+
+    tag: {
+      pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+      noindex: true,
+      disabled: false,
+    },
+  },
+};
+
+export const SITE = { ...CONFIG, blog: undefined };
+export const BLOG = CONFIG.blog;
+export const DATE_FORMATTER = CONFIG.dateFormatter;
